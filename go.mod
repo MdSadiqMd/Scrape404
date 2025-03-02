26d1a2b82b0378c2936ceb9f37ea91f8c292a9d9
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-03-02 16:10:48 +0530

    🪵  configured logger

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MdSadiqMd/Scrape404
 
 go 1.23.3
+
+require github.com/go-chi/chi v1.5.5
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/MdSadiqMd/Scrape404/package/middleware"
+	"github.com/go-chi/chi"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	r := chi.NewRouter()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	r.Use(middleware.Logging(logger))
 }
